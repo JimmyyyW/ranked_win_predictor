629fd7b362849a9dfe09fb2392cc9edb0a2fe611
JimmyyyW <50302042+JimmyyyW@users.noreply.github.com>
2019-05-27 13:34:10 +0100

    Adding web changes

--- ranked_win_predictor/predictor/models.py
+++ ranked_win_predictor/predictor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Predict(models.Model):
+    summoner = models.CharField(max_length=15)
+    predict = models.CharField(max_length=4)
+    accuracy = models.FloatField(max_length=4)
+    result = models.BooleanField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.summoner
 # Create your models here.
--- ranked_win_predictor/predictor/views.py
+++ ranked_win_predictor/predictor/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
-# Create your views here.
-predicts = [
-    {
-        'summoner': 'JimmyyW', #httpPOST summonername used as parameter for functionality
-        'predict': 'blue', #result from neural net
-        'result': 'correct', #validation on the above
-        'date': 'April 04, 2019', #logged date of instance
-        'confidence': '88'
-    },
-    {
-        'summoner': 'stozer',
-        'predict': 'blue',
-        'result': 'incorrect',
-        'date': 'April 04, 2019',
-        'confidence': '80'
-    }
-]
+from .models import Predict
 
 def home(request):
     context = {
-        'predicts': predicts
+        'predicts': Predict.objects.all()
     }
     return render(request, 'predictor/home.html', context)
 
